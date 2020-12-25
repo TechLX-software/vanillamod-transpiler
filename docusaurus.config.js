6ed857dca7050cde008bedfeca48f07a8684a8b4
Baker Sharp <bakers@stanford.edu>
2020-12-25 11:53:23 -0700

    fixed edit page in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,13 +96,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/bakerfugu/vanillamod-transpiler/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
